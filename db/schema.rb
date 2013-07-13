05437440c85a192d942311ca0d3a04f735745797
David Vega <david.vega@crowdint.com>
2013-07-13 12:39:28 -0700

    micropost table created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130713192957) do
+ActiveRecord::Schema.define(:version => 20130713193520) do
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
