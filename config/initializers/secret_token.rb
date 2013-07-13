7a1f5eea2814229e8c0feb8f592f7c774b8b4f2a
David Vega <david.vega@crowdint.com>
2013-07-13 12:29:34 -0700

    initial configuration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '744aaf12391a76e3a5b758d4911f131409ebcad96a64119dd811399c9838e5495881d7e051a1e745a511acf62b25937f6677a79f82b1f8821d02035cae1f9c13'
